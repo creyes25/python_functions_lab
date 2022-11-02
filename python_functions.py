85d2bafba93e4168458ff299d1890e518159201d
creyes25 <carolina.reyes2022@gmail.com>
2022-11-02 17:19:31 -0500

    complete challenge 2

--- python_functions.py
+++ python_functions.py
@@ -12,3 +12,16 @@ print(sum_to(6))
 print(sum_to(10))
 
 
+#? 2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+def largest(nums):
+  large = 0
+  for num in nums:
+    if num > large:
+      large = num
+  return large
+
+print(largest([1,3,6,4, 0]))
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
+
